c303cfcff5873c6e95eaf90565e57dc67ed45349
donghyun-daniel <donghyun.main@gmail.com>
2021-01-05 23:41:22 +0900

    requirements updated

--- webproject/webproject/settings.py
+++ webproject/webproject/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'h%b+4vd6a-gj!xs6*e&v@gy%mrp-=rwohfq@kwobsm=vtcyxh4'
 DEBUG = True
 
 # 주소를 넣어주면, 주소에 대해 이 장고 프로젝트에 진행하게 함
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ""
+]
 
 
 # Application definition
